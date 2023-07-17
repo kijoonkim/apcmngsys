4cc2757ebb938c9c08885fe29370a426411cc8e2
John <jeochu@gmail.com>
2023-07-17 10:15:32 +0900

    jsp 헤더 설정

--- src/main/resources/static/resource/script/common.js
+++ src/main/resources/static/resource/script/common.js
@@ -3,17 +3,40 @@
  *
  */
 
-/** @type {string} */
+/**
+ * js constant
+ */
+
+/** 
+ * @description 공통코드 url
+ * @type {string}
+ */
 const URL_COM_CDS = "/co/cd/comCdDtls";
 
-/** @type {string} */
+
+/**
+ * @type {string}
+ * @description blank string
+ */
 const STR_BLANK = "";
+
+/**
+ * @type {object}
+ * @description default header
+ */
 const postHeaders = {
     "Content-Type": "application/json"
 }
 
+
 /**
- * set post header programId
+ * global function
+ */
+
+/**
+ * @name gfn_setSysPrgrmId
+ * @description set post header programId
+ * @function
  * @param {string} sysPrgrmId
  */
 const gfn_setSysPrgrmId = function(sysPrgrmId) {
@@ -21,9 +44,10 @@ const gfn_setSysPrgrmId = function(sysPrgrmId) {
 }
 
 /**
- * async post by json
+ * @name gfn_postJSON
+ * @description async post by json
+ * @function
  * @param {string} _url
- * @param {*} _param
  * @param {object} _param
  * @param {[string]} sysPrgrmId
  * @returns {any}
@@ -61,7 +85,9 @@ async function gfn_postJSON(_url, _param, sysPrgrmId) {
 }
 
 /**
- * 공통코드 상세 리스트 가져오기
+ * @name gfn_getComCdDtls
+ * @description 공통코드 상세 리스트 가져오기
+ * @function
  * @param {string} _cdId	- 공통코드
  * @param {string} _apcCd	- APC코드
  * @returns {any[]}
@@ -73,44 +99,11 @@ async function gfn_getComCdDtls (_cdId, _apcCd = "0000") {
 	return JSON.stringify(data.resultList);
 }
 
-/**
- * sbux-select 데이터 설정
- * @param {string} _gridId
- * @param {any[]} _jsondataRef
- * @param {string} _cdId
- * @param {string} _apcCd
- * @returns {void}
- */
-async function gfn_setComCdGridSelect(_gridId, _jsondataRef, _cdId, _apcCd) {
-
-	if (gfn_isEmpty(_cdId)) {
-		return;
-	}
-
-
-	const postJsonPromise = gfn_postJSON(URL_COM_CDS, {cdId: _cdId, apcCd: _apcCd});
-	const data = await postJsonPromise;
-	console.log("cdDtls", data);
-
-	try {
-		_jsondataRef.length = 0;
-		data.resultList.forEach((item) => {
-			const cdVl = {
-				label: item.cdVlNm,
-				value: item.cdVl
-			}
-			_jsondataRef.push(cdVl);
-		});
-		SBUxMethod.refresh(_name);
-	} catch (e) {
-
-	}
-
-}
-
 
 /**
- * sbux-select 데이터 설정
+ * @name gfn_setComCdSBSelect
+ * @description sbux-select 데이터 설정
+ * @function
  * @param {(string|string[])} _gridIdList
  * @param {any[]} _jsondataRef
  * @param {string} _cdId
@@ -149,38 +142,77 @@ async function gfn_setComCdSBSelect(_gridIdList, _jsondataRef, _cdId, _apcCd) {
 	}
 }
 
+/**
+ * @name gfn_setComCdGridSelect
+ * @description sbgrid-select 데이터 설정
+ * @function
+ * @param {string} _gridId
+ * @param {any[]} _jsondataRef
+ * @param {string} _cdId
+ * @param {string} _apcCd
+ * @returns {void}
+ */
+async function gfn_setComCdGridSelect(_gridId, _jsondataRef, _cdId, _apcCd) {
+
+	if (gfn_isEmpty(_cdId)) {
+		return;
+	}
+
+	const postJsonPromise = gfn_postJSON(URL_COM_CDS, {cdId: _cdId, apcCd: _apcCd});
+	const data = await postJsonPromise;
+
+	try {
+		_jsondataRef.length = 0;
+		data.resultList.forEach((item) => {
+			const cdVl = {
+				label: item.cdVlNm,
+				value: item.cdVl
+			}
+			_jsondataRef.push(cdVl);
+		});
+		SBUxMethod.refresh(_gridId);
+	} catch (e) {
+	}
+}
+
 
 
 /**
- *  빈값 체크 함수
- * @param {any} str
+ * utility
+ */
+
+/**
+ * @name gfn_isEmpty
+ * @description 빈값 체크 함수
+ * @function
+ * @param {any} obj
  * @return {boolean}
  */
-const gfn_isEmpty = function (str) {
+const gfn_isEmpty = function (obj) {
 
-    if (typeof str == "boolean") {
-        return str;
-    } else if (typeof str == "string") {
-        str = str.trim();
+    if (typeof obj == "boolean") {
+        return obj;
+    } else if (typeof obj == "string") {
+        obj = obj.trim();
     }
 
-    if (str == undefined) {
+    if (obj == undefined) {
         return true;
-    } else if (str == "undefined") {
+    } else if (obj == "undefined") {
         return true;
-    } else if (str == null) {
+    } else if (obj == null) {
         return true;
-    } else if (str == "null") {
+    } else if (obj == "null") {
     	return true;
-    } else if (str == "") {
-        if (typeof str == "string" || typeof str == "object") {
+    } else if (obj == "") {
+        if (typeof obj == "string" || typeof obj == "object") {
             return true;
         } else {
             return false;
         }
-    } else if ("x" + str == "xNaN") {
+    } else if ("x" + obj == "xNaN") {
         return true;
-    } else if (str.length == 0) {
+    } else if (obj.length == 0) {
         return true;
     } else {
         return false;
@@ -188,7 +220,9 @@ const gfn_isEmpty = function (str) {
 }
 
 /**
- * String NVL
+ * @name gfn_nvl
+ * @description String NVL 처리
+ * @function
  * @param {string} str
  * @param {string} defaultStr
  * @return {string}
@@ -208,51 +242,69 @@ const gfn_nvl = function (str, defaultStr) {
 
 
 /**
- * 주어진 이름의 쿠키를 반환
- * 조건에 맞는 쿠키가 없다면 undefined 를 반환
- * @param {string} name
+ * @name gfn_getCookie
+ * @description 주어진 이름의 쿠키를 반환
+ * @description 조건에 맞는 쿠키가 없다면 undefined 를 반환
+ * @function
+ * @param {string} _name
  * @returns {string}
  */
-const gfn_getCookie = function (name) {
+const gfn_getCookie = function (_name) {
+	
 	let matches = document.cookie.match(new RegExp(
-		"(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[]\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+		"(?:^|; )" + _name.replace(/([\.$?*|{}\(\)\[]\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
 	));
+	
 	return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
 /**
- * set cookie
+ * @name gfn_setCookie
+ * @description set cookie
+ * @function
+ * @param {string} _name
+ * @param {any} _value
+ * @param {[any]} _options
+ * @returns {void}
  */
-const gfn_setCookie = function (name, value, options = {}) {
-	options = {
+const gfn_setCookie = function (_name, _value, _options = {}) {
+	_options = {
 		path: '/',
 		// 필요한 경우, 옵션 기본값을 설정할 수도 있습니다.
 		...options
 	};
 
-	if (options.expires instanceof Date) {
-		options.expires = options.expires.toUTCString();
+	if (_options.expires instanceof Date) {
+		_options.expires = _options.expires.toUTCString();
 	}
 
-	console.log("name", name);
-	console.log("value", value);
-	console.log("options", options);
+	console.log("name", _name);
+	console.log("value", _value);
+	console.log("options", _options);
 
-	let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
-	for ( let optionKey in options ) {
+	let updatedCookie = encodeURIComponent(_name) + "=" + encodeURIComponent(_value);
+	for ( let optionKey in _options ) {
 		updatedCookie += "; " + optionKey;
-		let optionValue = options[optionKey];
+		let optionValue = _options[optionKey];
 		if (optionValue !== true) {
 			updatedCookie += "=" + optionValue;
 		}
 	}
-
+	
 	document.cookie = updatedCookie;
-	console.log(document.cookie);
 }
 
+/**
+ * @name gv_comMsgList
+ * @description 공통메시지코드 정보 global variable
+ */
 const gv_comMsgList = [];
 
+/**
+ * @name gfn_getComMsgList
+ * @description get 공통메시지
+ * @function
+ */
 const gfn_getComMsgList = async function() {
 
 	const comMsgPromise = gfn_postJSON("/co/msg/comMsgs", {delYn: "N"});
@@ -261,23 +313,23 @@ const gfn_getComMsgList = async function() {
     try {
     	gv_comMsgList.length = 0;
     	data.resultList.forEach((item) => {
-			const menu = {
+			const msg = {
 				msgKey: item.msgKey,
 				msgCn: item.msgCn,
 				rmrk: item.rmrk,
 				msgKnd: item.msgKnd,
 				msgKndNm: item.msgKndNm
 			}
-			gv_comMsgList.push(menu);
+			gv_comMsgList.push(msg);
 		});
-
-		console.log("gv_comMsgList", gv_comMsgList);
-
 	} catch (e) {
 	}
 }
 
 /**
+ * @name gfn_getComMsg
+ * @description 공통메시지 호출
+ * @function
  * @param {string} _msgKey
  * @param {string[]} _arguments
  * @returns {string}
@@ -303,6 +355,31 @@ const gfn_getComMsg = function (_msgKey, ..._arguments) {
 
 	return msg;
 }
+
+/**
+ * @name gfn_comAlert
+ * @description 공통 alert
+ * @function
+ * @param {string} _msgKey
+ * @param {string[]} _arguments
+ */
+const gfn_comAlert = function (_msgKey, ..._arguments) {
+	return alert(gfn_getComMsg(_msgKey, _arguments));
+}
+
+/**
+ * @name gfn_comConfirm
+ * @description 공통 confirm
+ * @function
+ * @param {string} _msgKey
+ * @param {string[]} _arguments
+ * @returns {boolean}
+ */
+const gfn_comConfirm = function (_msgKey, ..._arguments) {
+	return confirm(gfn_getComMsg(_msgKey, _arguments));
+}
+
+
 
 /**
  * 실행부
