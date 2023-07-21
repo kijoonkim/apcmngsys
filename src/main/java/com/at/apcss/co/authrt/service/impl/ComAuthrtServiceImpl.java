90486a75c07bc6e72b11ca0932d8e2fa8209589f
John <jeochu@gmail.com>
2023-07-21 13:17:58 +0900

    R권한그룹관리 최신화

--- src/main/java/com/at/apcss/co/authrt/service/impl/ComAuthrtServiceImpl.java
+++ src/main/java/com/at/apcss/co/authrt/service/impl/ComAuthrtServiceImpl.java
@@ -1,9 +1,11 @@
 package com.at.apcss.co.authrt.service.impl;
 
+import java.util.HashMap;
 import java.util.List;
-
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.at.apcss.co.authrt.mapper.ComAuthrtMapper;
 import com.at.apcss.co.authrt.service.ComAuthrtService;
@@ -11,10 +13,14 @@ import com.at.apcss.co.authrt.vo.ComAuthrtMenuVO;
 import com.at.apcss.co.authrt.vo.ComAuthrtUiVO;
 import com.at.apcss.co.authrt.vo.ComAuthrtUserVO;
 import com.at.apcss.co.authrt.vo.ComAuthrtVO;
+import com.at.apcss.co.constants.ComConstants;
+import com.at.apcss.co.sys.service.impl.BaseServiceImpl;
 
 @Service("comAuthrtService")
-public class ComAuthrtServiceImpl implements ComAuthrtService {
+public class ComAuthrtServiceImpl extends BaseServiceImpl implements ComAuthrtService {
 
+	
+	
 	@Autowired
 	private ComAuthrtMapper comAuthrtMapper;
 	
@@ -45,6 +51,37 @@ public class ComAuthrtServiceImpl implements ComAuthrtService {
 		return rtnList;
 	}
 
+
+	@Override
+	public int insertComAuthrt(ComAuthrtVO comAuthVO) throws Exception {
+		
+		int insertedCnt = comAuthrtMapper.insertComAuthrt(comAuthVO);
+		
+		return insertedCnt;
+	}
+
+	@Override
+	public int updateComAuthrt(ComAuthrtVO comAuthVO) throws Exception {
+		
+		int updatedCnt = comAuthrtMapper.updateComAuthrt(comAuthVO);
+		
+		return updatedCnt;
+	}
+
+	@Override
+	public int deleteComAuthrt(ComAuthrtVO comAuthVO) throws Exception {
+		
+		int deletedCnt = comAuthrtMapper.deleteComAuthrt(comAuthVO);
+		
+		return deletedCnt;
+	}
+	
+	@Override
+	public ComAuthrtMenuVO selectComAuthrtMenu(ComAuthrtMenuVO comAuthrtMenuVO) throws Exception {
+		
+		return comAuthrtMapper.selectComAuthrtMenu(comAuthrtMenuVO);
+	}
+	
 	@Override
 	public List<ComAuthrtMenuVO> selectComAuthrtMenuList(ComAuthrtMenuVO comAuthrtMenuVO) {
 		
@@ -79,20 +116,26 @@ public class ComAuthrtServiceImpl implements ComAuthrtService {
 
 	@Override
 	public int insertComAuthrtMenu(ComAuthrtMenuVO comAuthrtMenuVO) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		int insertedCnt = comAuthrtMapper.insertComAuthrtMenu(comAuthrtMenuVO);
+		
+		return insertedCnt;
 	}
 
 	@Override
 	public int updateComAuthrtMenu(ComAuthrtMenuVO comAuthrtMenuVO) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		int updatedCnt = comAuthrtMapper.updateComAuthrtMenu(comAuthrtMenuVO);
+		
+		return updatedCnt;
 	}
 
 	@Override
 	public int deleteComAuthrtMenu(ComAuthrtMenuVO comAuthrtMenuVO) throws Exception {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		int deletedCnt = comAuthrtMapper.deleteComAuthrtMenu(comAuthrtMenuVO);
+		
+		return deletedCnt;
 	}
 
 	@Override
@@ -118,5 +161,62 @@ public class ComAuthrtServiceImpl implements ComAuthrtService {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+
+	@Override
+	public HashMap<String, Object> insertComAuthrtMenuList(ComAuthrtVO comAuthrtVO) throws Exception {
+		
+		logger.debug("$$$$$	333	$$$$$$$$$$$$$$");
+		
+		List<ComAuthrtMenuVO> comAuthrtMenuList = comAuthrtVO.getComAuthrtMenuList();
+		
+		logger.debug("$$$$$	444	$$$$$$$$$$$$$$");
+		if (comAuthrtMenuList == null) {
+			logger.debug("comAuthrtMenuList is null");
+		}
+		logger.debug("comAuthrtMenuList.size {}", comAuthrtMenuList.size());
+		
+		for ( ComAuthrtMenuVO menu : comAuthrtMenuList ) {
+			
+			ComAuthrtMenuVO comAuthrtMenuVO = new ComAuthrtMenuVO();
+			BeanUtils.copyProperties(comAuthrtVO, comAuthrtMenuVO);
+			
+			BeanUtils.copyProperties(menu, comAuthrtMenuVO,
+						ComConstants.PROP_COL_AUTHRT_ID,
+						ComConstants.PROP_SYS_FRST_INPT_DT,
+						ComConstants.PROP_SYS_FRST_INPT_USER_ID,
+						ComConstants.PROP_SYS_FRST_INPT_PRGRM_ID,
+						ComConstants.PROP_SYS_LAST_CHG_DT,
+						ComConstants.PROP_SYS_LAST_CHG_USER_ID,
+						ComConstants.PROP_SYS_LAST_CHG_PRGRM_ID
+					);
+			logger.debug("$$$$$	555	$$$$$$$$$$$$$$");
+			ComAuthrtMenuVO orgnVO = selectComAuthrtMenu(comAuthrtMenuVO);
+			logger.debug("$$$$$	666	$$$$$$$$$$$$$$");
+			boolean voExists = orgnVO != null && StringUtils.hasText(orgnVO.getAuthrtId());
+			// FIXME ComAuthUi 정보 등록			
+			if (ComConstants.CON_YES.equals(menu.getUseYn())) {
+				// 기등록 메뉴id 인지 확인
+				if (voExists) {
+					// update
+					// updateComAuthrtMenu(comAuthrtMenuVO);
+				} else {
+					// insert
+					insertComAuthrtMenu(comAuthrtMenuVO);
+				}
+			} else {
+				if (voExists) {
+					// delete 권한메뉴
+					deleteComAuthrtMenu(comAuthrtMenuVO);
+				}
+			}
+			
+			logger.debug("$$$$$	aaaa	$$$$$$$$$$$$$$");
+		}
+		
+		return null;
+	}
+
+
+
 
 }
---
+++ src/main/java/com/at/apcss/co/sys/service/impl/BaseServiceImpl.java
@@ -0,0 +1,8 @@
+package com.at.apcss.co.sys.service.impl;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public abstract class BaseServiceImpl {
+	public final Logger logger = LoggerFactory.getLogger(this.getClass());
+}
