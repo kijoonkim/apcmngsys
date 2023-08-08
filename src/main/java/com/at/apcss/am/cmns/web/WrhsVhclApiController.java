adbbf5dd2f66fe271259cac2e27d3f161c479b37
HaMinwoo <haminu0108@gmail.com>
2023-08-08 17:22:32 +0900

    add untracked file (src/main/java/com/at/apcss/am/cmns/web/WrhsVhclApiController.java)

---
+++ src/main/java/com/at/apcss/am/cmns/web/WrhsVhclApiController.java
@@ -0,0 +1,46 @@
+package com.at.apcss.am.cmns.web;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.at.apcss.am.cmns.service.WrhsVhclService;
+import com.at.apcss.am.cmns.vo.WrhsVhclVO;
+import com.at.apcss.co.constants.ComConstants;
+import com.at.apcss.co.sys.controller.BaseController;
+
+@RestController
+public class WrhsVhclApiController extends BaseController {
+	// 차량정보
+	@Resource(name = "wrhsVhclService")
+	private WrhsVhclService wrhsVhclService;
+	
+	// APC 차량 목록 조회
+	@PostMapping(value = "/am/cmns/wrhsVhcls", consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_HTML_VALUE })
+	public ResponseEntity<HashMap<String, Object>> selectApcVrtyList(@RequestBody WrhsVhclVO wrhsVhclVO, HttpServletRequest request) throws Exception {
+
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+		List<WrhsVhclVO> resultList = new ArrayList<>();
+
+		try {
+
+			resultList = wrhsVhclService.selectWrhsVhclList(wrhsVhclVO);
+
+		} catch (Exception e) {
+			return getErrorResponseEntity(e);
+		}
+
+		resultMap.put(ComConstants.PROP_RESULT_LIST, resultList);
+
+		return getSuccessResponseEntity(resultMap);
+	}
+}
