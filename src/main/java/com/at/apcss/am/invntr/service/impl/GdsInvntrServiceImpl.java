4fbdf15bbc465ee16a6bd5d748fb6e4d2c4f292b
John <jeochu@gmail.com>
2023-08-21 13:55:44 +0900

    상품재고 출하처리 및 취소 추가

--- src/main/java/com/at/apcss/am/invntr/service/impl/GdsInvntrServiceImpl.java
+++ src/main/java/com/at/apcss/am/invntr/service/impl/GdsInvntrServiceImpl.java
@@ -5,10 +5,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.at.apcss.am.invntr.mapper.GdsInvntrMapper;
 import com.at.apcss.am.invntr.service.GdsInvntrService;
 import com.at.apcss.am.invntr.vo.GdsInvntrVO;
+import com.at.apcss.co.sys.service.impl.BaseServiceImpl;
+import com.at.apcss.co.sys.util.ComUtil;
 
 /**
  * @Class Name : GdsInvntrServiceImpl.java
@@ -26,7 +29,7 @@ import com.at.apcss.am.invntr.vo.GdsInvntrVO;
  * </pre>
  */
 @Service("gdsInvntrService")
-public class GdsInvntrServiceImpl implements GdsInvntrService {
+public class GdsInvntrServiceImpl extends BaseServiceImpl implements GdsInvntrService {
 
 
 	@Autowired
@@ -84,6 +87,78 @@ public class GdsInvntrServiceImpl implements GdsInvntrService {
 			if (insertedCnt != 0) {
 
 			}
+		}
+
+		return null;
+	}
+
+	@Override
+	public HashMap<String, Object> updateGdsInvntrSpmtPrfmnc(GdsInvntrVO gdsInvntrVO) throws Exception {
+
+		GdsInvntrVO invntrInfo = selectGdsInvntr(gdsInvntrVO);
+
+		if (invntrInfo == null || !StringUtils.hasText(invntrInfo.getPckgno())) {
+			logger.debug("상품재고 없음");
+			return ComUtil.getResultMap("W0005", "상품재고");
+		}
+
+		if (gdsInvntrVO.getSpmtWght() > invntrInfo.getInvntrWght()) {
+			logger.debug("상품재고 대비 출하량 over");
+			return ComUtil.getResultMap("W0008", "재고량||출하량");		// W0008	{0} 보다 {1}이/가 큽니다.
+		}
+
+		// 재고량
+		int invntrQntt = invntrInfo.getInvntrQntt() - gdsInvntrVO.getSpmtQntt();
+		double invntrWght = invntrInfo.getInvntrWght() - gdsInvntrVO.getSpmtWght();
+		gdsInvntrVO.setInvntrQntt(invntrQntt);
+		gdsInvntrVO.setInvntrWght(invntrWght);
+
+		// 포장량
+		int spmtQntt = invntrInfo.getSpmtQntt() + gdsInvntrVO.getSpmtQntt();
+		double spmtWght = invntrInfo.getSpmtWght() + gdsInvntrVO.getSpmtWght();
+		gdsInvntrVO.setSpmtQntt(spmtQntt);
+		gdsInvntrVO.setSpmtWght(spmtWght);
+
+		int updatedCnt = gdsInvntrMapper.updateGdsInvntrSpmtPrfmnc(gdsInvntrVO);
+
+		if (updatedCnt != 1) {
+
+		}
+
+		return null;
+	}
+
+	@Override
+	public HashMap<String, Object> updateGdsInvntrSpmtPrfmncCncl(GdsInvntrVO gdsInvntrVO) throws Exception {
+
+		GdsInvntrVO invntrInfo = selectGdsInvntr(gdsInvntrVO);
+
+		if (invntrInfo == null || !StringUtils.hasText(invntrInfo.getPckgno())) {
+			logger.debug("상품재고정보 없음");
+			return ComUtil.getResultMap("W0005", "상품재고정보");
+		}
+
+		if (gdsInvntrVO.getSpmtWght() > invntrInfo.getSpmtWght()) {
+			logger.debug("상품재고 대비 출하량 over");
+			return ComUtil.getResultMap("W0008", "출하량||취소량");		// W0008	{0} 보다 {1}이/가 큽니다.
+		}
+
+		// 재고량
+		int invntrQntt = invntrInfo.getInvntrQntt() + gdsInvntrVO.getSpmtQntt();
+		double invntrWght = invntrInfo.getInvntrWght() + gdsInvntrVO.getSpmtWght();
+		gdsInvntrVO.setInvntrQntt(invntrQntt);
+		gdsInvntrVO.setInvntrWght(invntrWght);
+
+		// 포장량
+		int spmtQntt = invntrInfo.getSpmtQntt() - gdsInvntrVO.getSpmtQntt();
+		double spmtWght = invntrInfo.getSpmtWght() - gdsInvntrVO.getSpmtWght();
+		gdsInvntrVO.setSpmtQntt(spmtQntt);
+		gdsInvntrVO.setSpmtWght(spmtWght);
+
+		int updatedCnt = gdsInvntrMapper.updateGdsInvntrSpmtPrfmnc(gdsInvntrVO);
+
+		if (updatedCnt != 1) {
+
 		}
 
 		return null;
