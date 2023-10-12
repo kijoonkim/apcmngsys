138b4e4be65e430bd96e63aba5db6dc343ebe712
HaMinwoo <haminu0108@gmail.com>
2023-10-12 15:27:20 +0900

    상품입고등록 - 등록 기능 구현

--- src/main/java/com/at/apcss/am/wrhs/service/impl/GdsWrhsServiceImpl.java
+++ src/main/java/com/at/apcss/am/wrhs/service/impl/GdsWrhsServiceImpl.java
@@ -5,13 +5,19 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.egovframe.rte.fdl.cmmn.exception.EgovBizException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
+import com.at.apcss.co.sys.service.impl.BaseServiceImpl;
+import com.at.apcss.am.cmns.service.CmnsGdsService;
 import com.at.apcss.am.cmns.service.CmnsTaskNoService;
+import com.at.apcss.am.cmns.vo.CmnsGdsVO;
 import com.at.apcss.am.wrhs.mapper.GdsWrhsMapper;
 import com.at.apcss.am.invntr.vo.GdsInvntrVO;
 import com.at.apcss.am.wrhs.service.GdsWrhsService;
+import com.at.apcss.co.sys.util.ComUtil;
 
 /**
  * @Class Name : GdsWrhsServiceImpl.java
@@ -29,13 +35,16 @@ import com.at.apcss.am.wrhs.service.GdsWrhsService;
  * </pre>
  */
 @Service("gdsWrhsService")
-public class GdsWrhsServiceImpl implements GdsWrhsService {
+public class GdsWrhsServiceImpl extends BaseServiceImpl implements GdsWrhsService {
 	
 	@Autowired
 	private GdsWrhsMapper gdsWrhsMapper;
 	
 	@Resource(name = "cmnsTaskNoService")
 	private CmnsTaskNoService cmnsTaskNoService;
+	
+	@Resource(name="cmnsGdsService")
+	private CmnsGdsService cmnsGdsService;
 	
 	@Override
 	public List<GdsInvntrVO> selectGdsWrhsList(GdsInvntrVO gdsInvntrVO) throws Exception {
@@ -47,16 +56,37 @@ public class GdsWrhsServiceImpl implements GdsWrhsService {
 	
 	@Override
 	public HashMap<String, Object> insertGdsInvntr(GdsInvntrVO gdsInvntrVO) throws Exception {
-		int sn = 3;
+		int sn = 1;
 		String wrhsSeCd = "4";
 		String trsprtSeCd = "3";
 		String delYn = "N";
 		String pckgno = cmnsTaskNoService.selectPckgno(gdsInvntrVO.getApcCd(), gdsInvntrVO.getPckgYmd());
 		
+		CmnsGdsVO cmnsGdsVO = new CmnsGdsVO();
+		
+		cmnsGdsVO.setSysLastChgUserId(gdsInvntrVO.getSysLastChgUserId());
+		cmnsGdsVO.setSysLastChgPrgrmId(gdsInvntrVO.getSysLastChgPrgrmId());
+		cmnsGdsVO.setApcCd(gdsInvntrVO.getApcCd());
+		cmnsGdsVO.setItemCd(gdsInvntrVO.getItemCd());
+		cmnsGdsVO.setVrtyCd(gdsInvntrVO.getVrtyCd());
+		cmnsGdsVO.setSpcfctCd(gdsInvntrVO.getSpcfctCd());
+		
+		HashMap<String, Object> getGdsCd = cmnsGdsService.insertCheckGdsCd(cmnsGdsVO);
+		if (getGdsCd != null) {
+			throw new EgovBizException(getMessageForMap(getGdsCd));
+		}
+
+		if (!StringUtils.hasText(cmnsGdsVO.getNewGdsCd())) {
+			throw new EgovBizException(
+						getMessageForMap(ComUtil.getResultMap("E0003", "상품코드 발번"))
+					);	// {0} 시 오류가 발생하였습니다.
+		}
+		
 		gdsInvntrVO.setPckgno(pckgno);
 		gdsInvntrVO.setPckgSn(sn);
 		gdsInvntrVO.setWrhsSeCd(wrhsSeCd);
 		gdsInvntrVO.setTrsprtSeCd(trsprtSeCd);
+		gdsInvntrVO.setGdsCd(cmnsGdsVO.getNewGdsCd());
 		gdsInvntrVO.setDelYn(delYn);
 		
 		gdsWrhsMapper.insertGdsInvntr(gdsInvntrVO);
