7b89a12cfbf53720bab07aa86a608343fc9a0f2a
John <jeochu@gmail.com>
2023-11-13 09:18:03 +0900

    재고 detail vo 등록

---
+++ src/main/java/com/at/apcss/am/invntr/vo/InvntrDtlVO.java
@@ -0,0 +1,11 @@
+package com.at.apcss.am.invntr.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InvntrDtlVO {
+	private String invntrno;
+	private int invntrSn;
+}
