09299261026a4fce23947e8aaa8d971a1f3862b3
huno0419 <hayeedb@gmail.com>
2023-12-15 10:30:50 +0900

    출하관리 컨트롤러 수정

--- src/main/java/com/at/apcss/am/invntr/web/GdsInvntrController.java
+++ src/main/java/com/at/apcss/am/invntr/web/GdsInvntrController.java
@@ -49,6 +49,11 @@ public class GdsInvntrController extends BaseController {
 		} catch (Exception e) {
 			logger.debug(ComConstants.ERROR_CODE, e.getMessage());
 			return getErrorResponseEntity(e);
+		} finally {
+			HashMap<String, Object> rtnObj = setMenuComLog(request);
+			if (rtnObj != null) {
+				return getErrorResponseEntity(rtnObj);
+			}
 		}
 
 		resultMap.put(ComConstants.PROP_RESULT_LIST, resultList);
