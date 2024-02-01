694b2839b85fbfc25996cb7d38dba48493c10784
JUN\82105 <jj2977657@naver.com>
2024-02-01 11:32:14 +0900

    생산자조직 필터링 변수 추가

--- src/main/java/com/at/apcss/pd/aom/vo/PrdcrCrclOgnReqMngVO.java
+++ src/main/java/com/at/apcss/pd/aom/vo/PrdcrCrclOgnReqMngVO.java
@@ -14,6 +14,9 @@ import lombok.ToString;
 public class PrdcrCrclOgnReqMngVO extends ComVO{
 	private String apoCd;
 	private String apoSe;
+	private String frmhsHld;
+	private String frmhsHldYn;
+	private String frmhsCnt;
 	private String wrtYn;
 	private String userType;
 
