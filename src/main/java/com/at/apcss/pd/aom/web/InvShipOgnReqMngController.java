31ee4b85ca2c9d3d287ff76f798fce04f8a3d363
JUN\82105 <jj2977657@naver.com>
2024-02-06 18:02:37 +0900

    출자출하조직 신청관리 로우데이터 다운로드 추가

--- src/main/java/com/at/apcss/pd/aom/web/InvShipOgnReqMngController.java
+++ src/main/java/com/at/apcss/pd/aom/web/InvShipOgnReqMngController.java
@@ -26,94 +26,109 @@ public class InvShipOgnReqMngController extends BaseController{
 	@Resource(name= "InvShipOgnReqMngService")
 	private InvShipOgnReqMngService InvShipOgnReqMngService;
 
-//화면이동
+	//화면이동
 	@RequestMapping(value = "/pd/aom/InvShipOgnReqMng.do")
 	public String InvShipOgnReqMng() {
 		return "apcss/pd/aom/InvShipOgnReqMng";
 	}
 
-// 조회
-		@PostMapping(value = "/pd/aom/selectInvShipOgnReqMngList.do", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.TEXT_HTML_VALUE})
-		public ResponseEntity<HashMap<String, Object>> selectInvShipOgnReqMngList(Model model, @RequestBody InvShipOgnReqMngVO InvShipOgnReqMngVO, HttpServletRequest request) throws Exception{
-			HashMap<String,Object> resultMap = new HashMap<String,Object>();
-			List<InvShipOgnReqMngVO> resultList = new ArrayList<>();
-			try {
-				 resultList = InvShipOgnReqMngService.selectInvShipOgnReqMngList(InvShipOgnReqMngVO);
-			} catch (Exception e) {
-				logger.debug(e.getMessage());
-				return getErrorResponseEntity(e);
-			}
-			resultMap.put(ComConstants.PROP_RESULT_LIST, resultList);
-			return getSuccessResponseEntity(resultMap);
+	// 조회
+	@PostMapping(value = "/pd/aom/selectInvShipOgnReqMngList.do", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.TEXT_HTML_VALUE})
+	public ResponseEntity<HashMap<String, Object>> selectInvShipOgnReqMngList(Model model, @RequestBody InvShipOgnReqMngVO InvShipOgnReqMngVO, HttpServletRequest request) throws Exception{
+		HashMap<String,Object> resultMap = new HashMap<String,Object>();
+		List<InvShipOgnReqMngVO> resultList = new ArrayList<>();
+		try {
+			 resultList = InvShipOgnReqMngService.selectInvShipOgnReqMngList(InvShipOgnReqMngVO);
+		} catch (Exception e) {
+			logger.debug(e.getMessage());
+			return getErrorResponseEntity(e);
+		}
+		resultMap.put(ComConstants.PROP_RESULT_LIST, resultList);
+		return getSuccessResponseEntity(resultMap);
+	}
+
+
+
+	//등록
+	@PostMapping(value = "/pd/aom/insertInvShipOgnReqMng.do", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_HTML_VALUE})
+	public ResponseEntity<HashMap<String, Object>> insertInvShipOgnReqMng(@RequestBody InvShipOgnReqMngVO InvShipOgnReqMngVO, HttpServletRequest requset) throws Exception{
+		HashMap<String,Object> resultMap = new HashMap<String,Object>();
+
+		// validation check
+
+		// audit 항목
+		InvShipOgnReqMngVO.setSysFrstInptUserId(getUserId());
+		InvShipOgnReqMngVO.setSysFrstInptPrgrmId(getPrgrmId());
+		InvShipOgnReqMngVO.setSysLastChgUserId(getUserId());
+		InvShipOgnReqMngVO.setSysLastChgPrgrmId(getPrgrmId());
+
+		int insertedCnt = 0;
+
+		try {
+			insertedCnt = InvShipOgnReqMngService.insertInvShipOgnReqMng(InvShipOgnReqMngVO);
+		} catch (Exception e) {
+			logger.debug(e.getMessage());
+			return getErrorResponseEntity(e);
 		}
 
+		resultMap.put(ComConstants.PROP_INSERTED_CNT, insertedCnt);
+
+		return getSuccessResponseEntity(resultMap);
+	}
 
 
-		//등록
-		@PostMapping(value = "/pd/aom/insertInvShipOgnReqMng.do", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_HTML_VALUE})
-		public ResponseEntity<HashMap<String, Object>> insertInvShipOgnReqMng(@RequestBody InvShipOgnReqMngVO InvShipOgnReqMngVO, HttpServletRequest requset) throws Exception{
-			HashMap<String,Object> resultMap = new HashMap<String,Object>();
+	@PostMapping(value = "/pd/aom/multiSaveInvShipOgnReqMngList.do", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.TEXT_HTML_VALUE})
+	public ResponseEntity<HashMap<String, Object>> multiSaveInvShipOgnReqMngList(@RequestBody List<InvShipOgnReqMngVO> InvShipOgnReqMngVOList, HttpServletRequest request) throws Exception {
 
-			// validation check
+		HashMap<String,Object> resultMap = new HashMap<String,Object>();
 
-			// audit 항목
-			InvShipOgnReqMngVO.setSysFrstInptUserId(getUserId());
-			InvShipOgnReqMngVO.setSysFrstInptPrgrmId(getPrgrmId());
-			InvShipOgnReqMngVO.setSysLastChgUserId(getUserId());
-			InvShipOgnReqMngVO.setSysLastChgPrgrmId(getPrgrmId());
-
-			int insertedCnt = 0;
-
-			try {
-				insertedCnt = InvShipOgnReqMngService.insertInvShipOgnReqMng(InvShipOgnReqMngVO);
-			} catch (Exception e) {
-				logger.debug(e.getMessage());
-				return getErrorResponseEntity(e);
+		int savedCnt = 0;
+		try {
+			for (InvShipOgnReqMngVO InvShipOgnReqMngVO : InvShipOgnReqMngVOList) {
+				InvShipOgnReqMngVO.setSysFrstInptPrgrmId(getPrgrmId());
+				InvShipOgnReqMngVO.setSysFrstInptUserId(getUserId());
+				InvShipOgnReqMngVO.setSysLastChgPrgrmId(getPrgrmId());
+				InvShipOgnReqMngVO.setSysLastChgUserId(getUserId());
 			}
 
-			resultMap.put(ComConstants.PROP_INSERTED_CNT, insertedCnt);
-
-			return getSuccessResponseEntity(resultMap);
+			savedCnt = InvShipOgnReqMngService.multiSaveInvShipOgnReqMngList(InvShipOgnReqMngVOList);
+		}catch (Exception e) {
+			return getErrorResponseEntity(e);
 		}
 
+		resultMap.put(ComConstants.PROP_SAVED_CNT, savedCnt);
+		return getSuccessResponseEntity(resultMap);
+	}
 
-		@PostMapping(value = "/pd/aom/multiSaveInvShipOgnReqMngList.do", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.TEXT_HTML_VALUE})
-		public ResponseEntity<HashMap<String, Object>> multiSaveInvShipOgnReqMngList(@RequestBody List<InvShipOgnReqMngVO> InvShipOgnReqMngVOList, HttpServletRequest request) throws Exception {
+	@PostMapping(value = "/pd/aom/deleteInvShipOgnReqMng.do", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.TEXT_HTML_VALUE})
+	public ResponseEntity<HashMap<String, Object>> deleteInvShipOgnReqMng(@RequestBody InvShipOgnReqMngVO InvShipOgnReqMngVO, HttpServletRequest request) throws Exception {
+		logger.debug("/pd/aom/deleteInvShipOgnReqMng >>> 호출 >>> ");
 
-			HashMap<String,Object> resultMap = new HashMap<String,Object>();
-
-			int savedCnt = 0;
-			try {
-				for (InvShipOgnReqMngVO InvShipOgnReqMngVO : InvShipOgnReqMngVOList) {
-					InvShipOgnReqMngVO.setSysFrstInptPrgrmId(getPrgrmId());
-					InvShipOgnReqMngVO.setSysFrstInptUserId(getUserId());
-					InvShipOgnReqMngVO.setSysLastChgPrgrmId(getPrgrmId());
-					InvShipOgnReqMngVO.setSysLastChgUserId(getUserId());
-				}
-
-				savedCnt = InvShipOgnReqMngService.multiSaveInvShipOgnReqMngList(InvShipOgnReqMngVOList);
-			}catch (Exception e) {
-				return getErrorResponseEntity(e);
-			}
-
-			resultMap.put(ComConstants.PROP_SAVED_CNT, savedCnt);
-			return getSuccessResponseEntity(resultMap);
+		int result = 0;
+		try {
+			result =+ InvShipOgnReqMngService.deleteInvShipOgnReqMng(InvShipOgnReqMngVO);
+		}catch (Exception e) {
+			return getErrorResponseEntity(e);
 		}
 
-		@PostMapping(value = "/pd/aom/deleteInvShipOgnReqMng.do", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.TEXT_HTML_VALUE})
-		public ResponseEntity<HashMap<String, Object>> deleteInvShipOgnReqMng(@RequestBody InvShipOgnReqMngVO InvShipOgnReqMngVO, HttpServletRequest request) throws Exception {
-			logger.debug("/pd/aom/deleteInvShipOgnReqMng >>> 호출 >>> ");
+		HashMap<String,Object> resultMap = new HashMap<String,Object>();
+		resultMap.put("result", result);
+		return getSuccessResponseEntity(resultMap);
+	}
 
-			int result = 0;
-			try {
-				result =+ InvShipOgnReqMngService.deleteInvShipOgnReqMng(InvShipOgnReqMngVO);
-			}catch (Exception e) {
-				return getErrorResponseEntity(e);
-			}
-
-			HashMap<String,Object> resultMap = new HashMap<String,Object>();
-			resultMap.put("result", result);
-			return getSuccessResponseEntity(resultMap);
+	// 로우데이터 조회
+	@PostMapping(value = "/pd/aom/hiddenGrdIsoSelectList.do", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.TEXT_HTML_VALUE})
+	public ResponseEntity<HashMap<String, Object>> hiddenGrdIsoSelectList(Model model, @RequestBody InvShipOgnReqMngVO InvShipOgnReqMngVO, HttpServletRequest request) throws Exception{
+		HashMap<String,Object> resultMap = new HashMap<String,Object>();
+		List<InvShipOgnReqMngVO> resultList = new ArrayList<>();
+		try {
+			 resultList = InvShipOgnReqMngService.hiddenGrdIsoSelectList(InvShipOgnReqMngVO);
+		} catch (Exception e) {
+			logger.debug(e.getMessage());
+			return getErrorResponseEntity(e);
 		}
+		resultMap.put(ComConstants.PROP_RESULT_LIST, resultList);
+		return getSuccessResponseEntity(resultMap);
+	}
 
 }
