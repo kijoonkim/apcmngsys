65dec5a46137732a69677094416974936345aafb
LKH <funniple2021@gmail.com>
2024-06-04 17:08:44 +0900

    캘린더, 캘린더 생성 화면 추가

--- src/main/java/com/at/apcma/hr/hrt/com/web/ApcMaHrb1100Controller.java
+++ src/main/java/com/at/apcma/hr/hrt/com/web/ApcMaHrb1100Controller.java
@@ -1,7 +1,20 @@
 package com.at.apcma.hr.hrt.com.web;
 
+import com.at.apcma.com.service.ApcMaCommDirectService;
 import com.at.apcss.co.sys.controller.BaseController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * 캘린더생성을 처리하는 컨트롤러 클래스
@@ -21,4 +34,37 @@ import org.springframework.stereotype.Controller;
  */
 @Controller
 public class ApcMaHrb1100Controller extends BaseController {
+    @Resource(name= "apcMaCommDirectService")
+    private ApcMaCommDirectService apcMaCommDirectService;
+
+    @PostMapping(value = "/hr/hrt/com/selectHrb1100List.do", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.TEXT_HTML_VALUE})
+    public ResponseEntity<HashMap<String, Object>> selectHri1000List(
+            @RequestBody Map<String, Object> param
+            , Model model
+            , HttpSession session
+            , HttpServletRequest request) throws Exception{
+
+        logger.info("=============selectHrb1100List=====start========");
+        HashMap<String,Object> resultMap = new HashMap<String,Object>();
+
+        try {
+
+            param.put("procedure", 		"P_HRB1100_Q");
+            resultMap = apcMaCommDirectService.callProc(param, session, request, "");
+
+        } catch (Exception e) {
+            logger.debug(e.getMessage());
+            return getErrorResponseEntity(e);
+        }
+
+        logger.info("=============selectHrb1100List=====end========");
+        if(resultMap.get("resultStatus").equals("E")) {
+            String errorCode = Optional.ofNullable(resultMap.get("v_errorCode")).orElse("").toString();
+            String errorStr = Optional.ofNullable(resultMap.get("resultMessage")).orElse("").toString();
+
+            return getErrorResponseEntity(errorCode, errorStr);
+        } else {
+            return getSuccessResponseEntity(resultMap);
+        }
+    }
 }
