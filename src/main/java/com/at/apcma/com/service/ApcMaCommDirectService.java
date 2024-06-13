2f38a47aa0c8e97bbdb4f1bd49875d040dc6ee33
DESKTOP-HG3J2RL\GGAMP <blackp55@naver.com>
2024-06-13 16:20:53 +0900

    다이렉트 서비스 수정

--- src/main/java/com/at/apcma/com/service/ApcMaCommDirectService.java
+++ src/main/java/com/at/apcma/com/service/ApcMaCommDirectService.java
@@ -1,7 +1,6 @@
 package com.at.apcma.com.service;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -37,7 +36,7 @@ public interface ApcMaCommDirectService {
 	 * @throws Exception
 	 */
 	public HashMap<String, Object> callProc(Map<String, Object> param, HttpSession session, HttpServletRequest request, String ptype) throws Exception;
-	
+
 	/**
 	 * 내부에서 공통으로 처리하는 Service
 	 * @param request
@@ -47,17 +46,13 @@ public interface ApcMaCommDirectService {
 	 */
 	public Map<String, Object> InnerCallProc2(Map<String, Object> param, String[][] plist) throws Exception;
 
-
 	/**
-	 * tree level 생성
-	 * @param node
-	 * @param deptMap
-	 * @param sortedDepartments
-	 * @param level
+	 * 내부에서 공통으로 수식을 확인하는 Service
+	 * @param request
+	 * @param rtype
 	 * @return
 	 * @throws Exception
 	 */
-	void buildTree(Map<String, Object> node, Map<String, Map<String, Object>> deptMap,
-								  List<Map<String, Object>> sortedDepartments, int level);
+	public HashMap<String, Object> checkFormula(Map<String, Object> param) throws Exception;
 
 }
