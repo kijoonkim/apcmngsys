1f969eb928f725270bbd0967df0cf85a62f7640b
DESKTOP-HG3J2RL\GGAMP <blackp55@naver.com>
2024-06-25 17:02:49 +0900

    급여 전표처리 화면 등록

---
+++ src/main/java/com/at/apcma/hr/pay/web/ApcMaHrp4200Controller.java
@@ -0,0 +1,208 @@
+package com.at.apcma.hr.pay.web;
+
+import com.at.apcma.com.service.ApcMaCommDirectService;
+import com.at.apcss.co.sys.controller.BaseController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * 급여 전표처리 컨트롤러 클래스
+ * @author 		인텔릭아이앤에스
+ * @since 		2024.06.21
+ * @version 	1.0
+ * @see
+ *
+ * <pre>
+ * << 급여 전표처리 정보(Modification Information) >>
+ *
+ *  수정일      수정자		수정내용
+ *  ----------	----------	---------------------------
+ *  2023.06.24  표주완     	최초 생성
+ *
+ *  </pre>
+ */
+@Controller
+public class ApcMaHrp4200Controller extends BaseController {
+
+    @Resource(name= "apcMaCommDirectService")
+    private ApcMaCommDirectService apcMaCommDirectService;
+
+    // 급여 전표처리기준 조회
+    @PostMapping(value = "/hr/hrp/pay/selectHrp4200List.do", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.TEXT_HTML_VALUE})
+    public ResponseEntity<HashMap<String, Object>> selectHrp2320List(
+            @RequestBody Map<String, Object> param
+            , Model model
+            , HttpSession session
+            , HttpServletRequest request) throws Exception{
+
+        logger.info("=============selectHrp4200List=====start========");
+        HashMap<String,Object> resultMap = new HashMap<String,Object>();
+
+        try {
+
+            param.put("procedure", "P_HRP4200_Q");
+            resultMap = apcMaCommDirectService.callProc(param, session, request, "");
+
+        } catch (Exception e) {
+            logger.debug(e.getMessage());
+            return getErrorResponseEntity(e);
+        }
+
+        logger.info("=============selectHrp4200List=====end========");
+        if (resultMap.get("resultStatus").equals("E")) {
+            String errorCode = Optional.ofNullable(resultMap.get("v_errorCode")).orElse("").toString();
+            String errorStr = Optional.ofNullable(resultMap.get("v_errorStr")).orElse("").toString();
+
+            return getErrorResponseEntity(errorCode, errorStr);
+        } else {
+            return getSuccessResponseEntity(resultMap);
+        }
+    }
+
+
+    // 급여 전표처리기준 신규저장
+    @PostMapping(value = "/hr/hrp/pay/insertHrp4200S2.do", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.TEXT_HTML_VALUE})
+    public ResponseEntity<HashMap<String, Object>> insertHrp4200(
+            @RequestBody Map<String, Object> param
+            , Model model
+            , HttpSession session
+            , HttpServletRequest request) throws Exception{
+
+        logger.info("=============insertHrp4200S2=====start========");
+        HashMap<String,Object> resultMap = new HashMap<String,Object>();
+
+        try {
+
+            param.put("procedure", "P_HRP4200_S2");
+            resultMap = apcMaCommDirectService.callProc(param, session, request, "");
+
+        } catch (Exception e) {
+            logger.debug(e.getMessage());
+            return getErrorResponseEntity(e);
+        }
+
+        logger.info("=============insertHrp4200S2=====end========");
+        if (resultMap.get("resultStatus").equals("E")) {
+            String errorCode = Optional.ofNullable(resultMap.get("v_errorCode")).orElse("").toString();
+            String errorStr = Optional.ofNullable(resultMap.get("v_errorStr")).orElse("").toString();
+
+            return getErrorResponseEntity(errorCode, errorStr);
+        } else {
+            return getSuccessResponseEntity(resultMap);
+        }
+    }
+
+
+    // 급여 전표처리 [급상여 회계처리 라인 그리드] 저장
+    @PostMapping(value = "/hr/hrp/pay/insertHrp4200S3.do", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.TEXT_HTML_VALUE})
+    public ResponseEntity<HashMap<String, Object>> insertHrp4200S3(
+            @RequestBody Map<String, Object> param
+            , Model model
+            , HttpSession session
+            , HttpServletRequest request) throws Exception{
+
+        logger.info("=============insertHrp4200S3=====start========");
+        HashMap<String,Object> resultMap = new HashMap<String,Object>();
+
+        try {
+            for(String key : param.keySet()){
+                if(key.contains("P_HRP4200_S3")) {
+                    if(param.get(key) instanceof List) {
+                        List<HashMap<String,Object>> listData = (List<HashMap<String, Object>>) param.get(key);
+                        listData.stream().forEach(d -> {
+                            try {
+                                d.put("procedure", 		key);
+                                d = apcMaCommDirectService.callProc(d, session, request, "");
+                                resultMap.put("result", d);
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+                        resultMap.put(key, listData);
+                    } else if (param.get(key) instanceof Map) {
+                        Map<String, Object> mapData = (Map<String, Object>) param.get(key);
+                        mapData.put("procedure", 		key);
+                        resultMap.put(key, apcMaCommDirectService.callProc(mapData, session, request, ""));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            logger.debug(e.getMessage());
+            return getErrorResponseEntity(e);
+        }
+
+        logger.info("=============insertHrp4200S3=====end========");
+        HashMap<String, Object> result = (HashMap<String, Object>) resultMap.get("result");
+        if (result.get("resultStatus").equals("E")) {
+            String errorCode = Optional.ofNullable(resultMap.get("v_errorCode")).orElse("").toString();
+            String errorStr = Optional.ofNullable(resultMap.get("v_errorStr")).orElse("").toString();
+
+            return getErrorResponseEntity(errorCode, errorStr);
+        } else {
+            return getSuccessResponseEntity(resultMap);
+        }
+    }
+
+    // 급여 전표처리
+    @PostMapping(value = "/hr/hrp/pay/insertHrp4200S.do", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.TEXT_HTML_VALUE})
+    public ResponseEntity<HashMap<String, Object>> insertHrp4200S(
+            @RequestBody Map<String, Object> param
+            , Model model
+            , HttpSession session
+            , HttpServletRequest request) throws Exception{
+
+        logger.info("=============insertHrp4200S=====start========");
+        HashMap<String,Object> resultMap = new HashMap<String,Object>();
+
+        try {
+            for(String key : param.keySet()){
+                if(key.contains("P_HRP4200_S")) {
+                    if(param.get(key) instanceof List) {
+                        List<HashMap<String,Object>> listData = (List<HashMap<String, Object>>) param.get(key);
+                        listData.stream().forEach(d -> {
+                            try {
+                                d.put("procedure", 		key);
+                                d = apcMaCommDirectService.callProc(d, session, request, "");
+                                resultMap.put("result", d);
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        });
+                        resultMap.put(key, listData);
+                    } else if (param.get(key) instanceof Map) {
+                        Map<String, Object> mapData = (Map<String, Object>) param.get(key);
+                        mapData.put("procedure", 		key);
+                        resultMap.put(key, apcMaCommDirectService.callProc(mapData, session, request, ""));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            logger.debug(e.getMessage());
+            return getErrorResponseEntity(e);
+        }
+
+        logger.info("=============insertHrp4200S=====end========");
+        HashMap<String, Object> result = (HashMap<String, Object>) resultMap.get("result");
+        if (result.get("resultStatus").equals("E")) {
+            String errorCode = Optional.ofNullable(result.get("v_errorCode")).orElse("").toString();
+            String errorMessage = Optional.ofNullable(result.get("resultMessage")).orElse("").toString();
+
+            return getErrorResponseEntity(errorCode, errorMessage);
+        } else {
+            return getSuccessResponseEntity(result);
+        }
+    }
+
+}
