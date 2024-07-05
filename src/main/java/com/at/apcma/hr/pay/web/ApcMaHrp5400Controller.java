c193e42d34c084942cc60a1fd5254aff6a4ba616
DESKTOP-HG3J2RL\GGAMP <blackp55@naver.com>
2024-07-05 10:11:56 +0900

    내역관리 화면 수정

--- src/main/java/com/at/apcma/hr/pay/web/ApcMaHrp5400Controller.java
+++ src/main/java/com/at/apcma/hr/pay/web/ApcMaHrp5400Controller.java
@@ -39,7 +39,7 @@ public class ApcMaHrp5400Controller extends BaseController {
     @Resource(name= "apcMaCommDirectService")
     private ApcMaCommDirectService apcMaCommDirectService;
 
-    // 급여 전표처리기준 조회
+    // 고용보험 내역 관리 조회
     @PostMapping(value = "/hr/hrp/pay/selectHrp5400List.do", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.TEXT_HTML_VALUE})
     public ResponseEntity<HashMap<String, Object>> selectHrp5400List(
             @RequestBody Map<String, Object> param
@@ -65,7 +65,7 @@ public class ApcMaHrp5400Controller extends BaseController {
 
     }
 
-    // 국민연금 변동항목반영 저장
+    // 고용보험 내역 관리 급여반영,취소
     @PostMapping(value = "/hr/hrp/pay/insertHrp5400S1.do", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.TEXT_HTML_VALUE})
     public ResponseEntity<HashMap<String, Object>> insertHrp5400S1(
             @RequestBody Map<String, Object> param
@@ -91,7 +91,7 @@ public class ApcMaHrp5400Controller extends BaseController {
 
     }
 
-    // 국민연금 내역 관리 저장 프로시저
+    // 고용보험 내역 관리 저장 체크
     @PostMapping(value = "/hr/hrp/pay/insertHrp5400S.do", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.TEXT_HTML_VALUE})
     public ResponseEntity<HashMap<String, Object>> insertHrp5400S(
             @RequestBody Map<String, Object> param
