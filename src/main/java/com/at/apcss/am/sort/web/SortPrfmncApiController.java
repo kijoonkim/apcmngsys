bb03f2822bf2c9f830fecedb3ce6f02d763c615d
LAPTOP-8I3EL5BD\lenovo <choejiho602@gmail.com>
2024-07-09 17:24:04 +0900

    모바일 선별실적등록 controller 추가, 모바일 출하포장단위 controller 추가

--- src/main/java/com/at/apcss/am/sort/web/SortPrfmncApiController.java
+++ src/main/java/com/at/apcss/am/sort/web/SortPrfmncApiController.java
@@ -7,6 +7,9 @@ import java.util.List;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
+import com.at.apcss.am.sort.service.SortMngService;
+import com.at.apcss.am.sort.vo.SortMngVO;
+import com.at.apcss.am.wrhs.vo.RawMtrWrhsVO;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,6 +42,9 @@ public class SortPrfmncApiController extends BaseController {
 	@Resource(name = "sortPrfmncService")
 	private SortPrfmncService sortPrfmncService;
 
+	@Resource(name = "sortMngService")
+	private SortMngService sortMngService;
+
 	// 모바일 선별실적조회 API
 	@PostMapping(value = "/api/mobile/am/sort/selectSortPrfmncList.do", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.TEXT_HTML_VALUE})
 	public ResponseEntity<HashMap<String, Object>> selectSortPrfmncList(@RequestBody SortPrfmncVO sortPrfmncVO, HttpServletRequest request) throws Exception {
@@ -49,6 +55,94 @@ public class SortPrfmncApiController extends BaseController {
 		try {
 			resultList = sortPrfmncService.selectSortPrfmncList(sortPrfmncVO);
 		} catch(Exception e) {
+			return getErrorResponseEntity(e);
+		} finally {
+			HashMap<String, Object> rtnObj = setMenuComLog(request);
+			if (rtnObj != null) {
+				return getErrorResponseEntity(rtnObj);
+			}
+		}
+
+		resultMap.put(ComConstants.PROP_RESULT_LIST, resultList);
+
+		return getSuccessResponseEntity(resultMap);
+	}
+
+	//모바일 선별실적 INSERT
+	@PostMapping(value = "/api/mobile/am/sort/insertSortPrfmnc.do", consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_HTML_VALUE })
+	public ResponseEntity<HashMap<String, Object>> insertSortPrfmnc(@RequestBody SortMngVO sortMngVO, HttpServletRequest request) throws Exception {
+
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+
+		try {
+//			sortMngVO.setSysFrstInptUserId(getUserId());
+//			sortMngVO.setSysFrstInptPrgrmId(getPrgrmId());
+//			sortMngVO.setSysLastChgUserId(getUserId());
+//			sortMngVO.setSysLastChgPrgrmId(getPrgrmId());
+
+			sortMngVO.setNeedsInptRegYn(ComConstants.CON_YES);	// 투입실적 자동등록
+
+			HashMap<String, Object> rtnObj = sortMngService.insertSortPrfmnc(sortMngVO);
+			if (rtnObj != null) {
+				return getErrorResponseEntity(rtnObj);
+			}
+
+		} catch (Exception e) {
+			logger.debug(ComConstants.ERROR_CODE, e.getMessage());
+			return getErrorResponseEntity(e);
+		} finally {
+			HashMap<String, Object> rtnObj = setMenuComLog(request);
+			if (rtnObj != null) {
+				return getErrorResponseEntity(rtnObj);
+			}
+		}
+		resultMap.put(ComConstants.PROP_RESULT_MAP, sortMngVO);
+		return getSuccessResponseEntity(resultMap);
+	}
+
+	//모바일 선별실적 UPDATE
+	@PostMapping(value = "/api/mobile/am/sort/updateSortPrfmnc.do", consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_HTML_VALUE })
+	public ResponseEntity<HashMap<String, Object>> updateSortPrfmnc(@RequestBody SortMngVO sortMngVO, HttpServletRequest request) throws Exception {
+
+		HashMap<String, Object> resultMap = new HashMap<String, Object>();
+
+		try {
+//			sortMngVO.setSysFrstInptUserId(getUserId());
+//			sortMngVO.setSysFrstInptPrgrmId(getPrgrmId());
+//			sortMngVO.setSysLastChgUserId(getUserId());
+//			sortMngVO.setSysLastChgPrgrmId(getPrgrmId());
+
+			sortMngVO.setNeedsInptRegYn(ComConstants.CON_YES);	// 투입실적 자동등록
+
+			HashMap<String, Object> rtnObj = sortMngService.updateSortPrfmnc(sortMngVO);
+			if (rtnObj != null) {
+				return getErrorResponseEntity(rtnObj);
+			}
+
+		} catch (Exception e) {
+			logger.debug(ComConstants.ERROR_CODE, e.getMessage());
+			return getErrorResponseEntity(e);
+		} finally {
+			HashMap<String, Object> rtnObj = setMenuComLog(request);
+			if (rtnObj != null) {
+				return getErrorResponseEntity(rtnObj);
+			}
+		}
+		resultMap.put(ComConstants.PROP_RESULT_MAP, sortMngVO);
+		return getSuccessResponseEntity(resultMap);
+	}
+
+	//모바일 선별실적정보 조회 API
+	@PostMapping(value = "/api/mobile/am/sort/selectSortRsltList.do", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.TEXT_HTML_VALUE})
+	public ResponseEntity<HashMap<String, Object>> selectSortRsltList(@RequestBody SortPrfmncVO sortPrfmncVO, HttpServletRequest request) throws Exception {
+
+		HashMap<String,Object> resultMap = new HashMap<String,Object>();
+		List<SortPrfmncVO> resultList = new ArrayList<>();
+
+		try {
+			resultList = sortPrfmncService.selectSortListBySortno(sortPrfmncVO);
+		} catch(Exception e) {
+			logger.debug(ComConstants.ERROR_CODE, e.getMessage());
 			return getErrorResponseEntity(e);
 		} finally {
 			HashMap<String, Object> rtnObj = setMenuComLog(request);
