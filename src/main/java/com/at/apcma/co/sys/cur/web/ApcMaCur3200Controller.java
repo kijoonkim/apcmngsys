10d0bdc949c553ee329c05a77732d4520caff946
DESKTOP-UHQ6GNC\admin <dydwls1704@gmail.com>
2024-07-11 10:52:58 +0900

    통화정보 화면 신규 생성

---
+++ src/main/java/com/at/apcma/co/sys/cur/web/ApcMaCur3200Controller.java
@@ -0,0 +1,90 @@
+package com.at.apcma.co.sys.cur.web;
+
+import com.at.apcma.com.service.ApcMaComService;
+import com.at.apcma.com.service.ApcMaCommDirectService;
+import com.at.apcss.co.sys.controller.BaseController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.*;
+
+/**
+ * 통화 처리하는 컨트롤러 클래스
+ * @author 		인텔릭아이앤에스
+ * @since 		2024.07.09
+ * @version 	1.0
+ * @see
+ *
+ * <pre>
+ * << 통화 조회 >>
+ *
+ *  수정일      수정자		수정내용
+ *  ----------	----------	---------------------------
+ * @ 2024.07.09   	천용진		최초 생성
+ *
+ *  </pre>
+ */
+@Controller
+public class ApcMaCur3200Controller extends BaseController {
+
+	@Resource(name= "apcMaCommDirectService")
+	private ApcMaCommDirectService apcMaCommDirectService;
+	
+    @Resource(name= "apcMaComService")
+    private ApcMaComService apcMaComService;
+    
+	// 통화 조회
+	@PostMapping(value = "/co/sys/cur/selectCom3200.do", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.TEXT_HTML_VALUE})
+	public ResponseEntity<HashMap<String, Object>> selectCom3200(
+    		@RequestBody Map<String, Object> param
+			,Model model
+			,HttpSession session
+			,HttpServletRequest request) throws Exception{
+
+		logger.info("=============selectCom3200=====start========");
+		HashMap<String,Object> resultMap = new HashMap<String,Object>();
+
+		try {
+			
+			param.put("procedure", 		"P_COM3200_Q");
+			resultMap = apcMaCommDirectService.callProc(param, session, request, "");
+
+		} catch (Exception e) {
+			logger.debug(e.getMessage());
+			return getErrorResponseEntity(e);
+		}
+
+		logger.info("=============selectCom3200=====end========");
+		return getSuccessResponseEntityMa(resultMap);
+	}	
+	
+	// 통화 정보 저장
+	@PostMapping(value = "/co/sys/cur/insertCom3200.do", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.TEXT_HTML_VALUE})
+	public ResponseEntity<HashMap<String, Object>> insertCom3200(
+			@RequestBody Map<String, Object> param
+			,Model model
+			,HttpSession session
+			,HttpServletRequest request) throws Exception{
+		
+		logger.info("=============insertCom3200=====start========");
+		HashMap<String,Object> resultMap = new HashMap<String,Object>();
+		
+		try {
+			resultMap = apcMaComService.processForListData(param, session, request, "", "P_COM3200_S");
+					
+			logger.info("=============insertCom3200=====end========");
+			return getSuccessResponseEntityMa(resultMap);
+		} catch (Exception e) {
+			logger.debug(e.getMessage());
+			return getErrorResponseEntity(e);
+		}
+		
+	}	
+}
