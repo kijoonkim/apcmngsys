1f107cb7c7d10eb3532dc6a4585f16a52d64b045
DESKTOP-HG3J2RL\GGAMP <blackp55@naver.com>
2024-08-12 17:38:07 +0900

    연말정산 계산 화면 등록

---
+++ src/main/java/com/at/apcma/hr/hra/adj/web/ApcMaHra1500Controller.java
@@ -0,0 +1,71 @@
+package com.at.apcma.hr.hra.adj.web;
+
+import com.at.apcma.com.service.ApcMaComService;
+import com.at.apcma.com.service.ApcMaCommDirectService;
+import com.at.apcss.co.sys.controller.BaseController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 연말정산 계산 컨트롤러 클래스
+ * @author 		인텔릭아이앤에스
+ * @since 		2024.08.12
+ * @version 	1.0
+ * @see
+ *
+ * <pre>
+ * << 연말정산 계산(Modification Information) >>
+ *
+ *  수정일      수정자		수정내용
+ *  ----------	----------	---------------------------
+ *  2023.08.12  표주완     	최초 생성
+ *
+ *  </pre>
+ */
+@Controller
+public class ApcMaHra1500Controller extends BaseController {
+
+    @Resource(name = "apcMaCommDirectService")
+    private ApcMaCommDirectService apcMaCommDirectService;
+
+    @Resource(name = "apcMaComService")
+    private ApcMaComService apcMaComService;
+
+    // 정산자료(개인) 조회
+    @PostMapping(value = "/hr/hra/adj/selectHra1500List.do", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_HTML_VALUE})
+    public ResponseEntity<HashMap<String, Object>> selectHra1500List(
+            @RequestBody Map<String, Object> param
+            , Model model
+            , HttpSession session
+            , HttpServletRequest request) throws Exception {
+
+        logger.info("=============selectHra1500List=====start========");
+        HashMap<String, Object> resultMap = new HashMap<String, Object>();
+
+        try {
+
+            param.put("procedure", "P_HRA1500_Q");
+            resultMap = apcMaCommDirectService.callProc(param, session, request, "");
+
+        } catch (Exception e) {
+            logger.debug(e.getMessage());
+            return getErrorResponseEntity(e);
+        }
+
+        logger.info("=============selectHra1500List=====end========");
+        return getSuccessResponseEntityMa(resultMap);
+
+    }
+
+
+}
