1a46b5c62a9ed4918adf4f15c05f8d6922c396ab
DESKTOP-HG3J2RL\GGAMP <blackp55@naver.com>
2024-08-30 17:01:20 +0900

    연말정산 결과조회 화면 등록

---
+++ src/main/java/com/at/apcma/hr/hra/adj/web/ApcMaHra1710Controller.java
@@ -0,0 +1,94 @@
+package com.at.apcma.hr.hra.adj.web;
+
+import com.at.apcma.com.service.ApcMaComService;
+import com.at.apcma.com.service.ApcMaCommDirectService;
+import com.at.apcss.co.sys.controller.BaseController;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 연말정산 결과조회 컨트롤러 클래스
+ * @author 		인텔릭아이앤에스
+ * @since 		2024.08.22
+ * @version 	1.0
+ * @see
+ *
+ * <pre>
+ * << 연말정산 결과조회(Modification Information) >>
+ *
+ *  수정일      수정자		수정내용
+ *  ----------	----------	---------------------------
+ *  2023.09.05  표주완     	최초 생성
+ *
+ *  </pre>
+ */
+@Controller
+public class ApcMaHra1710Controller extends BaseController {
+
+    @Resource(name = "apcMaCommDirectService")
+    private ApcMaCommDirectService apcMaCommDirectService;
+
+    @Resource(name = "apcMaComService")
+    private ApcMaComService apcMaComService;
+
+    // 연말정산 결과조회 조회
+    @PostMapping(value = "/hr/hra/adj/selectHra1710List.do", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.TEXT_HTML_VALUE})
+    public ResponseEntity<HashMap<String, Object>> selectHra1710List(
+            @RequestBody Map<String, Object> param
+            , Model model
+            , HttpSession session
+            , HttpServletRequest request) throws Exception {
+
+        logger.info("=============selectHra1710List=====start========");
+        HashMap<String, Object> resultMap = new HashMap<String, Object>();
+
+        try {
+
+            param.put("procedure", "P_HRA1710_Q");
+            resultMap = apcMaCommDirectService.callProc(param, session, request, "");
+
+        } catch (Exception e) {
+            logger.debug(e.getMessage());
+            return getErrorResponseEntity(e);
+        }
+
+        logger.info("=============selectHra1710List=====end========");
+        return getSuccessResponseEntityMa(resultMap);
+
+    }
+
+    // 연말정산 결과조회 등록
+    @PostMapping(value = "/hr/hra/adj/insertHra1710.do", consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.TEXT_HTML_VALUE})
+    public ResponseEntity<HashMap<String, Object>> insertHra1710(
+            @RequestBody Map<String, Object> param
+            , Model model
+            , HttpSession session
+            , HttpServletRequest request) throws Exception{
+
+        logger.info("=============insertHra1710=====start========");
+        HashMap<String,Object> resultMap = new HashMap<String,Object>();
+
+        try {
+            resultMap = apcMaComService.processForListData(param, session, request, "", "P_HRA1710_S");
+
+            logger.info("=============insertHra1710=====end========");
+            return getSuccessResponseEntityMa(resultMap);
+        } catch (Exception e) {
+            logger.debug(e.getMessage());
+            return getErrorResponseEntity(e);
+        }
+    }
+
+
+
+}
