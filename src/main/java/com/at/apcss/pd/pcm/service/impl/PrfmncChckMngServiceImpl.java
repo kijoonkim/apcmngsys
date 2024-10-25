c67cc4506822a010b48a8e17f7415a8880b630ea
JUN\82105 <jj2977657@naver.com>
2024-10-25 09:40:27 +0900

    승인형 조직 총취급액 실적관리

---
+++ src/main/java/com/at/apcss/pd/pcm/service/impl/PrfmncChckMngServiceImpl.java
@@ -0,0 +1,55 @@
+package com.at.apcss.pd.pcm.service.impl;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.at.apcss.co.sys.service.impl.BaseServiceImpl;
+
+import com.at.apcss.pd.pcm.service.PrfmncChckMngService;
+import com.at.apcss.pd.pcm.vo.PrfmncChckMngVO;
+import com.at.apcss.pd.pcm.mapper.PrfmncChckMngMapper;
+
+
+/**
+ * @Class Name : PrfmncChckMngServiceImpl.java
+ * @Description : 게시판 서비스를 정의하기 위한 서비스 구현 클래스
+ * @author ljw
+ * @since 2024.10.24
+ * @version 1.0
+ * @see
+ *
+ * <pre>
+ * << 개정이력(Modification Information) >>
+ * 수정일        수정자         수정내용
+ * ----------  ----------  ---------------------------
+ * 2024.10.24  ljw         최초 생성
+ * </pre>
+ */
+@Service("PrfmncChckMngService")
+public class PrfmncChckMngServiceImpl extends BaseServiceImpl implements PrfmncChckMngService{
+
+	@Autowired
+	private PrfmncChckMngMapper PrfmncChckMngMapper;
+
+	@Override
+	public PrfmncChckMngVO selectUoAprv(PrfmncChckMngVO PrfmncChckMngVO) throws Exception {
+		PrfmncChckMngVO result = PrfmncChckMngMapper.selectUoAprv(PrfmncChckMngVO);
+		return result;
+	}
+
+	@Override
+	public List<PrfmncChckMngVO> selectUoAprvList(PrfmncChckMngVO PrfmncChckMngVO) throws Exception {
+		List<PrfmncChckMngVO> resultList = PrfmncChckMngMapper.selectUoAprvList(PrfmncChckMngVO);
+		return resultList;
+	}
+
+	@Override
+	public List<PrfmncChckMngVO> selectPrfmncChckMngList(PrfmncChckMngVO PrfmncChckMngVO) throws Exception {
+
+		List<PrfmncChckMngVO> resultList = PrfmncChckMngMapper.selectPrfmncChckMngList(PrfmncChckMngVO);
+
+		return resultList;
+	}
+
+}
---
+++ src/main/java/com/at/apcss/pd/pcm/vo/PrfmncChckMngVO.java
@@ -0,0 +1,41 @@
+package com.at.apcss.pd.pcm.vo;
+
+import com.at.apcss.co.sys.vo.ComVO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PrfmncChckMngVO extends ComVO{
+	private String yr;			//등록년도
+	private String apoCd;		//조직코드
+	private String apoSe;		//조직구분
+	private String brno;		//사업자등록번호
+	private String crno;		//법인등록번호
+	private String corpNm;		//법인명
+	private String aprv;		//승인형/육성형 구분
+
+	private String itemCd;		//품목코드
+	private String itemNm;		//품목명
+	private String itemNmList;	//보유 품목 리스트
+	private String slctnYr;		//승인연도
+
+	private String prfmncAmt1;		//1월실적
+	private String prfmncAmt2;		//2월실적
+	private String prfmncAmt3;		//3월실적
+	private String prfmncAmt4;		//4월실적
+	private String prfmncAmt5;		//5월실적
+	private String prfmncAmt6;		//6월실적
+	private String prfmncAmt7;		//7월실적
+	private String prfmncAmt8;		//8월실적
+	private String prfmncAmt9;		//9월실적
+	private String prfmncAmt10;		//10월실적
+	private String prfmncAmt11;		//11월실적
+	private String prfmncAmt12;		//12월실적
+
+	private String prevYrPrfmncAmt;	//전년도말 실적
+	private String expctPrfmncAmt;	//24년말 기준(예상치)
+	private String prfmncAmtRt;		//연말전망치 기준 증감률(%)
+
+}
