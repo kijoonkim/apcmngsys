ad7a1f684cbbbd563bc305f048335c340b8b5e9a
SonMinSeoung <dlgkfn4@naver.com>
2025-08-23 18:36:44 +0900

    websocket

--- src/main/java/egovframework/com/config/WebSocketConfig.java
+++ src/main/java/egovframework/com/config/WebSocketConfig.java
@@ -3,7 +3,6 @@ package egovframework.com.config;
 import egovframework.com.cmm.websoket.CustomHandshakeInterceptor;
 import egovframework.com.cmm.websoket.CustomWebSocketHandler;
 
-import jeus.spring.websocket.JeusRequestUpgradeStrategy; // 컴파일은 필요
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
@@ -11,6 +10,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.HandshakeHandler;
+import org.springframework.web.socket.server.RequestUpgradeStrategy;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
@@ -24,18 +24,15 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Bean
     public HandshakeHandler handshakeHandler() {
-        // JEUS 여부 판단: jeus.home 또는 커스텀 벤더 플래그
-        boolean isJeus =
-                System.getProperty("jeus.home") != null ||
-                        "jeus".equalsIgnoreCase(System.getProperty("custoum.server.vender"));
-
-        if (isJeus) {
-            // JEUS에서는 명시적으로 JEUS 전략을 사용 (DefaultHandshakeHandler 자동감지 못함)
-            return new DefaultHandshakeHandler(new JeusRequestUpgradeStrategy());
+        try {
+            Class<?> clazz = Class.forName("jeus.spring.websocket.JeusRequestUpgradeStrategy");
+            RequestUpgradeStrategy s =
+                    (RequestUpgradeStrategy) clazz.getDeclaredConstructor().newInstance();
+            return new DefaultHandshakeHandler(s);  // JEUS일 때
+        } catch (Throwable ignore) {
+            // 클래스가 없거나(로컬/톰캣) 만들어지지 않으면 기본 핸드셰이크로 폴백
+            return new DefaultHandshakeHandler();
         }
-
-        // Tomcat 등 일반 서블릿 컨테이너에서는 기본 핸들러가 자동으로 적절한 전략을 선택
-        return new DefaultHandshakeHandler();
     }
 
     @Override
