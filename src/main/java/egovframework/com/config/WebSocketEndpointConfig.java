6c2400c137029f62cbdb04848cf64b78d18d9d84
SonMinSeoung <dlgkfn4@naver.com>
2025-09-21 18:37:34 +0900

    websocket

---
+++ src/main/java/egovframework/com/config/WebSocketEndpointConfig.java
@@ -0,0 +1,16 @@
+package egovframework.com.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
+
+@Component
+public class WebSocketEndpointConfig {
+
+    @Bean
+    //웹 소켓 end point bean에 등록하기
+    public ServerEndpointExporter serverEndpointExporter() {
+
+        return new ServerEndpointExporter();
+    }
+}
