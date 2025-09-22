7d9a91c22b7ba49b964b6539b5f72ea44a468415
SonMinSeoung <dlgkfn4@naver.com>
2025-09-22 14:15:48 +0900

    embedded

--- src/main/java/egovframework/com/config/WebSocketEndpointConfig.java
+++ src/main/java/egovframework/com/config/WebSocketEndpointConfig.java
@@ -1,10 +1,12 @@
 package egovframework.com.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Component
+@Profile("embedded")
 public class WebSocketEndpointConfig {
 
     @Bean
