92609e3105be6c8e0e9f4436021c016eae4a0fc0
SonMinSeoung <dlgkfn4@naver.com>
2025-10-17 13:30:45 +0900

    websocket tomcat remove

--- src/main/java/egovframework/com/config/WebSocketEndpointConfig.java
+++ src/main/java/egovframework/com/config/WebSocketEndpointConfig.java
@@ -1,12 +1,14 @@
 package egovframework.com.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
-@Component
-@Profile({"local","dev"})
+@Configuration
+@ConditionalOnClass(name = "org.apache.tomcat.websocket.server.WsSci")
 public class WebSocketEndpointConfig {
 
     @Bean
